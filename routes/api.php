90a197dd93134b1384e19467aec9a78515231be1
Salman Zafar <salman.zafar@innowi.com>
2021-04-29 01:11:06 +0500

    route changes in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('todo')
     ->middleware(['auth:api'])
     ->group(function (){
-        Route::apiResource('todo', TodoController::class);
+        Route::apiResource('/', 'TodoController');
     });
