0cd7705c180e8ccac20dcf2a1ccf233892fed338
Salman Zafar <salman.zafar@innowi.com>
2021-04-29 16:33:10 +0500

    done with user login api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\VerifyEmailRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
@@ -12,6 +14,24 @@ class UserController extends Controller
     public function register(RegisterRequest $request): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         return $request->store();
+    }
+
+    public function login(LoginRequest $request): \Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        $isLoggedIn = auth()->attempt($request->validated());
+
+        if (!$isLoggedIn) {
+            return response([
+                'error' => 'Invalid credentials or Account suspended'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $token = Auth::user()
+            ->createToken(auth()->user()->name)->accessToken;
+
+        return response()->json([
+            'token' => 'Bearer '. $token,
+        ],Response::HTTP_OK);
     }
 
     public function verifyEmail(VerifyEmailRequest $request, $user): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')
     ->group(function (){
         Route::post('register', [UserController::class, 'register']);
+        Route::post('login', [UserController::class, 'login']);
         Route::post('verify-email/{user}', [UserController::class, 'verifyEmail']);
     });
 
